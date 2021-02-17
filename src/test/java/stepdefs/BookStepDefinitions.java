4de806659cfc0fec85de131cf3b30271534f267f
jjeurissen <jeroenjeurissen@gmail.com>
2021-02-17 17:22:33 +0100

    Cucumber/Rest assured upgrade

--- src/test/java/stepdefs/BookStepDefinitions.java
+++ src/test/java/stepdefs/BookStepDefinitions.java
@@ -4,10 +4,10 @@ import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
 
-import cucumber.api.java.en.And;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.And;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
@@ -23,7 +23,7 @@ public class BookStepDefinitions {
 
     private String ENDPOINT_GET_BOOK_BY_ISBN = "https://www.googleapis.com/books/v1/volumes";
 
-    @Given("a book exists with an isbn of (.*)")
+    @Given("^a book exists with an isbn of (.*)$")
     public void a_book_exists_with_isbn(String isbn) {
         request = given().param("q", "isbn:" + isbn);
     }
@@ -34,7 +34,7 @@ public class BookStepDefinitions {
         System.out.println("response: " + response.prettyPrint());
     }
 
-    @Then("the status code is (\\d+)")
+    @Then("^the status code is (\\d+)$")
     public void verify_status_code(int statusCode) {
         json = response.then().statusCode(statusCode);
     }
